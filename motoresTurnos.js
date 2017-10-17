9eb9c42620ccf5eb6322fab5ba685c89ca1f0aae
katiro <katirox9@gmail.com>
2017-10-17 00:20:08 -0600

    repartidorV3

--- motoresTurnos.js
+++ motoresTurnos.js
@@ -1,24 +1,49 @@
 // mecanismo de control de turnos
+function llamar(){
+  window.location='PokerHold.html';}
 //var iJugadores = 10;
-//var oJugadores = new Array();
+//creador de la matriz
+var oJugadores = new Array(10);
+for (var i = 0; i < 10; i++) {
+  oJugadores[i] = new Array(5);
+  for (var i2 = 0; i2 < 5; i2++) {
+    oJugadores[i][i2]="";
+  }};
+//function CrearArreglo(iJugadores) {
+//}
+//return oJugadores;
+//}
+//obtener jugadores
+
+function obtener() {
+  var iJuga = document.getElementById("jug").value;
+  //CrearArreglo(iJuga);
+  turnos(iJuga);
+}
+
+
+
+
 
 function turnos(iJugadores) {
   var turnos = 0;
-  while(turnos < 10){
-    switch (turnos) {
-      case 1:
-        preFloop(iJugadores);
-        break;
-      default:
 
-    }
-  }
+preFloop(iJugadores);
+//  while(turnos < 10){
+//    switch (turnos) {
+//      case 1:
+
+//        break;
+//      default:
+
+//    }
+//  }
 }
 //turno de pre-floop
 function preFloop(iJugadores) {
-  var oJugadores = new Array(iJugadores);
+  //var oJugadores = new Array(iJugadores);
   for (var i = 0; i < iJugadores; i++) {
-    oJugadores[i] = new Array(4);
+    //oJugadores[i] = new Array(4);
     for (var i2 = 2; i2 < 4; i2++) {
         oJugadores[i][i2]= repartidor();
     }
